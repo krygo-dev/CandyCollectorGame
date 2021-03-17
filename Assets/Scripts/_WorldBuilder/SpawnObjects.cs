0533390afcd6ce034d51639443c9cf97de858366
Adam Chylaszek <adam.chylaszek98@gmail.com>
2021-03-17 12:46:59 +0100

    World generator fix

--- Assets/Scripts/_WorldBuilder/SpawnObjects.cs
+++ Assets/Scripts/_WorldBuilder/SpawnObjects.cs
@@ -14,29 +14,34 @@ public class SpawnObjects : MonoBehaviour
         float randomPositionX, randomPositionY, randomPositionZ;
         Vector3 randomPosition;
 
-        for (int i = 0; i < objectsNumber; i++)
+        int i = 0;
+
+        while (i < objectsNumber)
         {
             randomPositionX = Random.Range(-440.0f, 440.0f);
             randomPositionZ = Random.Range(-440.0f, 440.0f);
             randomPositionY = 100.0f;
 
-            randomPosition = new Vector3(randomPositionX, randomPositionY, randomPositionZ);
-
-            if (Physics.Raycast(randomPosition, Vector3.down, out hit, 200.0f))
+            if (randomPositionX > -386 && randomPositionX < -142 && randomPositionZ > -13 && randomPositionZ < 337)
             {
-                randomPositionY = hit.point.y;
+                continue;
+            } 
+            else
+            {
+                randomPosition = new Vector3(randomPositionX, randomPositionY, randomPositionZ);
 
-                if (randomPositionY >= -1 && randomPositionY <= 22)
+                if (Physics.Raycast(randomPosition, Vector3.down, out hit, 200.0f))
                 {
-                    randomPosition = new Vector3(randomPositionX, randomPositionY, randomPositionZ);
+                    randomPositionY = hit.point.y;
 
-                    // No rotation, object perfectly aligned with with world or parent axes
-                    //Instantiate(gameObject, randomPosition, Quaternion.identity);
-
-                    // Random Y rotation, looks more realistic
-                    //gameObject.AddComponent<MeshCollider>(); //collision
-                    Instantiate(gameObject, randomPosition, Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0)), parent);
+                    if (randomPositionY >= -1 && randomPositionY <= 22)
+                    {
+                        randomPosition = new Vector3(randomPositionX, randomPositionY, randomPositionZ);
+                        Instantiate(gameObject, randomPosition, Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0)), parent);
+                    }
                 }
+
+                i++;
             }
         }
     }
