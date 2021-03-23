21f9f1b8ca6ec594c72d81cfedb8aa10499e0566
Adam Chylaszek <adam.chylaszek98@gmail.com>
2021-03-23 14:19:24 +0100

    Candy collecting functionality

---
+++ Assets/Scripts/CollectCandy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCandy : MonoBehaviour
+{
+    private int itemCount = 0;
+    
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Candy")
+        {
+            Destroy(col.gameObject);
+            itemCount++;
+            Debug.Log(itemCount);
+        }
+    }
+
+}
