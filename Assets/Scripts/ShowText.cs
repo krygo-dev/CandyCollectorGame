57bc10cf891cc0654d0749d3a4608298d3adbfdf
Adam Chylaszek <adam.chylaszek98@gmail.com>
2021-03-24 14:30:51 +0100

    Appearing text script

---
+++ Assets/Scripts/ShowText.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowText : MonoBehaviour
+{
+
+    public GameObject textField;
+    public GameObject obj;
+    public string textToShow = "Text";
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+
+    void Update()
+    {
+        if (textField != null && obj != null)
+        {
+            if (Vector3.Distance(obj.transform.position, this.transform.position) < 5)
+            {
+                textField.GetComponent<Text>().text = textToShow;
+            } 
+            else
+            {
+                textField.GetComponent<Text>().text = "";
+            }
+        }
+    }
+}
