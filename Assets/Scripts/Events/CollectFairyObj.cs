7f61c908979b081aa9852e01b9054fdaf9fad357
Adam Chylaszek <adam.chylaszek98@gmail.com>
2021-04-01 15:10:03 +0200

    Fable map beans collecting

---
+++ Assets/Scripts/Events/CollectFairyObj.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectFairyObj : MonoBehaviour
+{
+    public GameObject eventObj;
+
+    void Update()
+    {
+        if (eventObj != null)
+        {
+            float dist = Vector3.Distance(eventObj.transform.position, this.transform.position);
+
+            if (Input.GetButtonDown("Action") && dist < 5)
+            {
+                if (eventObj.tag == "RedBean")
+                {
+                    PlayerInfo.eventObj1 = true;
+                }
+
+                if (eventObj.tag == "BlueBean")
+                {
+                    PlayerInfo.eventObj2 = true;
+                }
+
+                if (eventObj.tag == "YellowBean")
+                {
+                    PlayerInfo.eventObj3 = true;
+                }
+
+                Destroy(eventObj);
+            }
+        }
+    }
+}
