cfb5d98e685abbbc7a0d30915fa005ec612306b6
AdamBarczyk <adambar1998@gmail.com>
2021-04-02 18:33:34 +0200

    sword animation and some fixes

--- Assets/Scripts/Events/SwordEventAnim.cs
+++ Assets/Scripts/Events/SwordEventAnim.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿/*
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -41,6 +42,61 @@ public class SwordEventAnim : MonoBehaviour
                     float newY = pos.y + height;
                     sword.transform.position = new Vector3(pos.x, newY, pos.z);
                 }
+            }
+        }
+    }
+}
+*/
+
+//wersja z gotową animacją
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordEventAnim : MonoBehaviour
+{
+    //public GameObject sword;
+    [SerializeField] private Animator sword = null;
+    [SerializeField] private bool swordPulledOut = false;
+    private int animationFrameCounter = 0;
+    private bool keyPressed = false;
+    private bool animationStarted = false;
+    public int reward = 100;
+    Vector3 pos;
+
+    void Update()
+    {
+        if (sword != null)
+        {
+            pos = sword.transform.position;
+
+            if (!swordPulledOut && (Input.GetButtonDown("Action") || keyPressed ) && Vector3.Distance(pos, this.transform.position) < 5)
+            {
+                keyPressed = true;
+                animationFrameCounter++;
+                Debug.Log(animationFrameCounter);
+
+                Debug.Log("przed");
+
+                if (!animationStarted)
+                {
+                    sword.Play("pull_out_sword", 0, 0.0f);
+                    animationStarted = true;
+                }
+                
+                if (animationFrameCounter >= 115)
+                {
+                    swordPulledOut = true;
+                    // Barczyko jak będziesz robić animację miecza to na samym końcu musi być coś takiego żeby gracz dostał nagrodę
+                    Destroy(sword.gameObject);
+                    //Destroy(sword.gameObject);
+                    PlayerInfo.collectedCandy += reward;
+
+                    // dla porzadku
+                    keyPressed = false;
+                    animationStarted = false;
+                }
+                Debug.Log("po");
             }
         }
     }
