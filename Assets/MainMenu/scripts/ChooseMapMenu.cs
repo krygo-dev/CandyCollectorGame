4aac82b8c156758108129ff7d689cd6bd5d029e3
AdamBarczyk <adambar1998@gmail.com>
2021-04-05 21:08:59 +0200

    Added main menu to the game

---
+++ Assets/MainMenu/scripts/ChooseMapMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseMapMenu : MonoBehaviour
+{
+    [SerializeField] string mapName;
+
+    public void LoadMap (string mapName)
+    {
+        SceneManager.LoadScene(mapName);
+    }
+}
