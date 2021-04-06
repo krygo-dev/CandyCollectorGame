31dad3c97c9b06001ab68ca8dcacdb3087eca67f
Adam Chylaszek <adam.chylaszek98@gmail.com>
2021-04-06 11:55:23 +0200

    Move car script, cars and food assets

---
+++ Assets/Scripts/Animations/MoveCar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCar : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private float rotation;
+
+    void Start()
+    {
+        rotation = transform.localRotation.eulerAngles.y;
+    }
+
+    void FixedUpdate()
+    {
+        if (rotation == 0 || rotation == 360)
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if (rotation == 90 || rotation == -270)
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+
+        if (rotation == 180 || rotation == -180)
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        if (rotation == -90 || rotation == 270)
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/Animations/WheelAnimation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelAnimation : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        transform.Rotate(0, 0, speed * Time.deltaTime);
+    }
+}
