aafd6afcf6ff1e16d4c621bd245553eec16a6c67
Adam Chylaszek <adam.chylaszek98@gmail.com>
2021-04-10 16:11:00 +0200

    City map events

---
+++ Assets/Scripts/Events/CollectCityObj.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCityObj : MonoBehaviour
+{
+    public GameObject eventObj;
+
+    void Update()
+    {
+        if (eventObj != null)
+        {
+            float dist = Vector3.Distance(eventObj.transform.position, this.transform.position);
+
+            if (Input.GetButtonDown("Action") && dist < 5)
+            {
+                if (eventObj.tag == "Pizza")
+                {
+                    PlayerInfo.eventObj1 = true;
+                }
+
+                if (eventObj.tag == "HotDog")
+                {
+                    PlayerInfo.eventObj2 = true;
+                }
+
+                if (eventObj.tag == "Fries")
+                {
+                    PlayerInfo.eventObj3 = true;
+                }
+
+                Destroy(eventObj);
+            }
+        }
+    }
+}
