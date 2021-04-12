658ee05c37356c7bf564cf8bd4b04a3a78ce041c
Adam Chylaszek <adam.chylaszek98@gmail.com>
2021-04-12 14:04:00 +0200

    Loading screens

--- Assets/MainMenu/scripts/ChooseMapMenu.cs
+++ Assets/MainMenu/scripts/ChooseMapMenu.cs
@@ -2,13 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ChooseMapMenu : MonoBehaviour
 {
     [SerializeField] string mapName;
 
+    public GameObject panel;
+    public Slider slider;
+
     public void LoadMap (string mapName)
     {
-        SceneManager.LoadScene(mapName);
+        StartCoroutine(LoadAsync(mapName));
+    }
+
+    IEnumerator LoadAsync(string mapName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(mapName);
+        panel.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+
+            yield return null;
+        }
     }
 }
