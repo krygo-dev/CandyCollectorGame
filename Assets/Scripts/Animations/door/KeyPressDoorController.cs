966956d723f65d59c29ecf47860bedf3df0781fb
AdamBarczyk <adambar1998@gmail.com>
2021-04-13 20:08:55 +0200

    Added door handling, some fixes

---
+++ Assets/Scripts/Animations/door/KeyPressDoorController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPressDoorController : MonoBehaviour
+{
+    [SerializeField] private Animator myDoor = null;
+    [SerializeField] private bool doorClosed = true;
+    public GameObject obj;
+    public int Distance = 5;
+
+
+    void Update()
+    {
+        if (Vector3.Distance(obj.transform.position, this.transform.position) < Distance)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+        {
+            if (doorClosed)
+            {
+                myDoor.Play("OpenDoor", 0, 0.0f);
+                doorClosed = !doorClosed;
+
+                //gameObject.SetActive(false);
+            }
+
+            else if (!doorClosed)
+            {
+                myDoor.Play("CloseDoor", 0, 0.0f);
+                doorClosed = !doorClosed;
+
+                //gameObject.SetActive(false);
+            }
+        }
+        }
+    }
+}
---
+++ Assets/Scripts/Events/DoorInteractionInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorInteractionInfo : MonoBehaviour
+{
+
+    public GameObject panel;
+    public GameObject eventObj;
+    public string textToShow = "Text";
+    public int Distance = 5;
+
+
+    void Update()
+    {
+        if (eventObj == null)
+        {
+            panel.GetComponent<Text>().text = "";
+        }
+
+
+        if (panel != null && eventObj != null)
+        {
+            if (Vector3.Distance(eventObj.transform.position, this.transform.position) < Distance)
+            {
+                panel.GetComponent<Text>().text = textToShow;
+            } 
+            else
+            {
+                panel.GetComponent<Text>().text = "";
+            }
+        }
+    }
+}
